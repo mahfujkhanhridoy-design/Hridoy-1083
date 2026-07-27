f86024cdc6fa3603724af1dc7970dbbbe2391156
Mahfuj khan khan <mahfujkhanhridoy@gmail.com>
2026-07-27 22:31:35 +0600

    Update and rename README.md to mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -52,15 +52,19 @@ void mergeSort(int arr[], int l, int r) {
 
 int main() {
     int n;
+
+    cout << "Enter the size of the array: ";
     cin >> n;
 
     int arr[n];
 
+    cout << "Enter the elements of the array: "<<endl;
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
     mergeSort(arr, 0, n - 1);
 
+    cout << "Sorted array: ";
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
 
